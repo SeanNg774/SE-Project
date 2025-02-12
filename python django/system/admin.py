b8bc5d3a888d8103b40e254b7bbe8e3362762f97
SeanNg774 <109175862+SeanNg774@users.noreply.github.com>
2025-02-12 08:04:39 +0800

    Update admin.py

--- python django/system/admin.py
+++ python django/system/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Admin, Teacher, Student, Fees
+from .models import Admin,Teacher, Student, Fee ,Accountant, Parent , Report , MonthlyInvoice , Class , Reminder , Payment
 
 admin.site.register(Admin)
 admin.site.register(Teacher)
 admin.site.register(Student)
-admin.site.register(Fees)
+admin.site.register(Fee)
+admin.site.register(Accountant)
+admin.site.register(Parent)
